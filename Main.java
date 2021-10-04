ed479b7aba7a3a4a7e6dba32caccbf8e46f0e747
anna sganderla <sganderlakarlaanna@gmail.com>
2021-10-04 20:11:20 -0300

    add more objects

--- Main.java
+++ Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
     Cinema cinema = new Cinema("cinemark", 1);
+    Cinema cinema2 = new Cinema("cinecataratas", 2);
 
-    Sessoes sessoes = new Sessoes(1,"Sexta-feira - 14:00",4);
+    Sessoes sessao = new Sessoes(1,"Sexta-feira - 14:00",4);
+    Sessoes sessao2 = new Sessoes(2,"Domingo - 20:00",5);
 
-    cinema.getSessoes().add(sessoes);
+    cinema.getSessoes().add(sessao);
+    cinema2.getSessoes().add(sessao2);
 
     System.out.println(cinema);
 
